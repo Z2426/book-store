3bb6cf94ebac6295feff094c831037b19f1c8320
leeanhvu <leeanhvu404@gmail.com>
2023-10-16 22:38:24 +0700

    frontend

---
+++ frontend/src/pages/DeleteBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const DeleteBook=()=>{
+    return (
+        <div>DeleteBooks</div>
+    )
+}
+export default DeleteBook
---
+++ frontend/src/pages/EditBook.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+const EditBook=()=>{
+    return (
+        <div>DeleteBooks</div>
+    )
+}
+export default EditBook
